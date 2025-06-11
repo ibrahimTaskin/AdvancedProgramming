1ac0ae8eec9fe246d245bcc36c73a57fcb274deb
ibrahim <ibrahim@DESKTOP-BSJFPIP>
2025-06-11 22:19:20 +0300

    Add project files.

---
+++ AdvancedProgramming/Program.cs
@@ -0,0 +1,34 @@
+﻿MyDelegateHandler myDelegate = (string name) => Greeting(name);
+myDelegate("Meryem"); // Invoke ederiz.
+
+MyEventPublisher m = new();
+m.MyEvent += RaiseText;
+m.RaiseEvent(); // MyEvent tetiklenir.
+
+
+GreetingHandler greetingHandler = () => Console.WriteLine("Welcome 1");
+greetingHandler += () => Console.WriteLine("Welcome 2");
+greetingHandler += () => Console.WriteLine("Welcome 3");
+greetingHandler += () => Console.WriteLine("Welcome 4");
+greetingHandler += () => Console.WriteLine("Welcome 5");
+greetingHandler(); // Invoke ederiz.
+
+Console.ReadLine();
+
+void Greeting(string name) => Console.WriteLine($"Hello " + name);
+void RaiseText() => Console.WriteLine("Text raised!");
+
+public delegate void MyDelegateHandler(string name);
+public delegate void GreetingHandler();
+
+
+
+class MyEventPublisher
+{
+    public delegate void XHandler();
+    public event XHandler MyEvent;
+    public void RaiseEvent()
+    {
+        MyEvent?.Invoke();
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,32 @@
+﻿string path = "D:\\Projects"; // Replace with your directory path
+
+PathControl pathControl = new PathControl();
+pathControl.PathControlEvent += (double fileSize) => Console.WriteLine($"Path size exceeded 10 MB! File size: {fileSize}");
+await pathControl.Control(path); // Replace with your directory path
+
+public class PathControl
+{
+    public delegate void PathHandler(double fileSize);
+    public event PathHandler PathControlEvent;
+
+    public async Task Control(string path)
+    {
+        while (true)
+        {
+            // Simulate some asynchronous operation
+            await Task.Delay(1000);
+
+            DirectoryInfo directoryInfo = new(path);
+            var files = directoryInfo.GetFiles("*.*", SearchOption.AllDirectories);
+            var size = await Task.Run(() => files.Sum(file => file.Length));
+            var sizeInMb = size / (1024.0 * 1024.0);
+
+            if (sizeInMb > 10)
+            {
+                // If the size exceeds 10 MB, invoke the event
+                PathControlEvent?.Invoke(sizeInMb); ;
+            }
+
+        }
+    }
+}
